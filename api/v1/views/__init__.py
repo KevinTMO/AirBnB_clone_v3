94740c0320ba0267b8f0d542c5e0dfccf65293e2
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-06 05:16:58 +0000

    Removing all the others imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,17 +6,8 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from models import storage
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.user import User
 
 # Import all files for handling RESTFul API actions of classes
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-# from api.v1.views.cities import *
-# from api.v1.views.amenities import *
-# from api.v1.views.users import *
-# from api.v1.views.places import *
-# from api.v1.views.reviews import *
