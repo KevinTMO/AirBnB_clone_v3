f9bf5bb2cbc4ca84b0bab4b425850627f84b4712
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-06 03:55:39 +0000

    Adding an error handler method in json style

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ Configuring flask for our app
 
 from api.v1.views import app_views
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from os import getenv
 
@@ -21,6 +21,16 @@ def tearApp(self):
     closing session query
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    """
+    Handlers errors 404
+    """
+    return jsonify({
+        'error': 'Not found'
+    })
 
 
 if __name__ == "__main__":
