980ba9878814e28d1152b032c8e349987774ada9
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-06 18:54:39 +0000

    Removing hash in import lines view.__init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,7 +7,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from models import storage
 from models.state import State
-# from models.city import City
+from models.city import City
 # from models.place import Place
 # from models.amenity import Amenity
 # from models.user import User
@@ -17,8 +17,8 @@ from models.state import State
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
 # from api.v1.views.amenities import *
-# from api.v1.views.cities import *
 # from api.v1.views.places import *
 # from api.v1.views.places_amenities import *
 # from api.v1.views.places_reviews import *
