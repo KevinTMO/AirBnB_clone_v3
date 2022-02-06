436d37b65ba1b67cc22bcdf450e55840cd915298
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-06 01:51:21 +0000

    Adding argument to teardown decorator method

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module: app.py
+
+Configuring flask for our app
+"""
+
+from api.v1.views import app_views
+from models import storage
+from flask import Flask
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tearApp(self):
+    """
+    closing session query
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
