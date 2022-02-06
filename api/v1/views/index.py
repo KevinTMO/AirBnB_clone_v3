402e46b61c227895645a35af6806bca2f0540adb
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-06 02:57:59 +0000

    Adding a route named stats to get the count of every object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,7 @@ Configuring a route to return JSON string
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -18,3 +19,19 @@ def status():
     return jsonify({
         'status': 'OK'
     })
+
+
+@app_views.route('/stats')
+def clsCount():
+    """
+    Retrieve the number of each objects by type
+    """
+    result = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(result)
