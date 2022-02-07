82063c561a1d15ebca13a1ac3062d52dee55057e
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-02-07 03:40:11 +0000

    import place

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from models import storage
 from models.state import State
 from models.city import City
-# from models.place import Place
+from models.place import Place
 from models.amenity import Amenity
 from models.user import User
 # from models.review import Review
@@ -18,8 +18,8 @@ from models.user import User
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-# from api.v1.views.amenities import *
-# from api.v1.views.places import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
 from api.v1.views.places_amenities import *
 # from api.v1.views.places_reviews import *
 from api.v1.views.users import *
