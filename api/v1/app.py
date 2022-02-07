137419903915c80727f15e0157c3bce34ffbf87e
ChristianRojasOliver <christian.rojas201@gmail.com>
2022-02-07 21:15:50 +0000

    adding cross-origin resource sharing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,27 @@
 #!/usr/bin/python3
-"""
-Module: app.py
-
-Configuring flask for our app
-"""
-
-from api.v1.views import app_views
+"""app.py to connect to API"""
+import os
 from models import storage
-from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tearApp(self):
-    """
-    closing session query
-    """
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def pageNotFound(error):
-    """
-    Handlers errors 404
-    """
-    return make_response(jsonify({
-        'error': 'Not found'
-    }), 404)
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
